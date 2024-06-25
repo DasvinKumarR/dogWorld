a7516824fe6ed3d2158b80c4feb09ad678e8cb47
DasvinKumarR <dasvinkumar2404@gmail.com>
2024-06-25 21:36:35 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// getting root content div element by using query selector
+let  root = document.querySelector(".content");
+// asyn promise function to get dog image data
+async function getImage(){
+    let response = await fetch('https://dog.ceo/api/breeds/image/random')
+    let data = await response.json();
+    let imageUrl = data.message;
+    root.innerHTML = `<img src="${imageUrl}" alt="Dog Image Loading..."/>`
+}
